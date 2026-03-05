f6899c56b8d88cdf4d027ea580c072d5a9b81c2c
Varun Verma <varun.verma2024@gmail.com>
2026-03-05 15:34:48 +0530

    tying to learn something, can't

--- internal/server/server.go
+++ internal/server/server.go
@@ -20,16 +20,20 @@ type Server struct {
 
 func NewServer() *http.Server {
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
-	NewServer := &Server{
+
+	// this is light wrapper around the std lib's http server
+	// So that it has our ports and db connection
+	myServer := &Server{
 		port: port,
 
 		db: database.New(),
 	}
 
 	// Declare Server config
+	// This is the actually engine: The standard lib's http server
 	server := &http.Server{
-		Addr:         fmt.Sprintf(":%d", NewServer.port),
-		Handler:      NewServer.RegisterRoutes(),
+		Addr:         fmt.Sprintf(":%d", myServer.port),
+		Handler:      myServer.RegisterRoutes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
